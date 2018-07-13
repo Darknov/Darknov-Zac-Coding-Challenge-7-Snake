53264443c519b466b22ea95669583efecb1fa952
Przemysław Maciaszek <darknov.dono@gmail.com>
2018-07-13 06:52:52 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,67 @@
+// temporarily
+const boxImg = document.getElementById("box");
 
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
 
 const player = {
 	x: 0,
 	y: 0,
+	velocity: {x:0, y:0},
 	boxes: [{x:0, y:0}, {x:0, y:0}, {x:0, y:0}, {x:0, y:0}],
 	render: function() {
 		for(let i = 0; i < this.boxes.length; i++) {
-			context.drawImage(boxImg, this.boxes[i].x, this.boxes[i].y);
+			context.save();
+			context.beginPath();
+			context.translate( this.boxes[i].x + 12, this.boxes[i].y + 12 );
+			// temporary
+			// we need to add rotation speed
+			// or something else
+			// so there is no instant rotation to the next box
+			let rotation = 0;
+			if(i == 0) {
+				rotation = Math.atan2(mouse.y - this.boxes[i].y - 12, mouse.x - this.boxes[i].x - 12)
+			} else {
+				rotation = Math.atan2(this.boxes[i-1].y - this.boxes[i].y, this.boxes[i-1].x - this.boxes[i].x)
+			}
+			context.rotate(rotation);
+			context.drawImage(boxImg, -12, -12);
+			context.restore();
 		}
 	},
 	update: function() {
-		this.boxes[0].x = mouse.x - 24;
-		this.boxes[0].y = mouse.y - 12;
+		// max velocity that snake can travel with
+		// temporary
+		// we will have to calculate sin or cos to
+		// get real max velocity
+		let maxVelocity = {x: 5, y: 5};
+		// velocity with mouse to 'smoothen' travel to mouse
+		let toMouseVelocity = {
+			x: (mouse.x - 24 - this.boxes[0].x)/5,
+			y: (mouse.y - 12 - this.boxes[0].y)/5
+		}
+		if(toMouseVelocity.x > 0) {
+			this.velocity.x = maxVelocity.x > toMouseVelocity.x ? toMouseVelocity.x : maxVelocity.x;
+		} else {
+			this.velocity.x = maxVelocity.x > -toMouseVelocity.x ? toMouseVelocity.x : -maxVelocity.x;
+		}
+		
+		if(toMouseVelocity.y > 0) {
+			this.velocity.y = maxVelocity.y > toMouseVelocity.y ? toMouseVelocity.y : maxVelocity.y;
+		} else {
+			this.velocity.y = maxVelocity.y > -toMouseVelocity.y ? toMouseVelocity.y : -maxVelocity.y;
+		}
+		
+		
+		
+		this.boxes[0].x += this.velocity.x;
+		this.boxes[0].y += this.velocity.y;
+
 		for(let i = 1; i < this.boxes.length; i++) {
-			this.boxes[i].x += (this.boxes[i-1].x - 24 - this.boxes[i].x)/2;
-			this.boxes[i].y += (this.boxes[i-1].y - this.boxes[i].y)/2;
+			let attachPointX = this.velocity.x >= 0 ? -12 : 12;
+			let attachPointY = this.velocity.y >= 0 ? -12 : 12;
+			this.boxes[i].x += (this.boxes[i-1].x + attachPointX - this.boxes[i].x)/2;
+			this.boxes[i].y += (this.boxes[i-1].y + attachPointY - this.boxes[i].y)/2;
 		}
 	}
 	
@@ -25,12 +72,7 @@ const mouse = {
 	y: 0
 }
 
-const assetsPath = "./assets/";
 
-const boxImg = document.getElementById("box");
-
-var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d");
 
 function clearCanvas() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
