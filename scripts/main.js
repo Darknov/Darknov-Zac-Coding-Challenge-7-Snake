bdb1c8617ddbf60ea996b236c13559a677da3072
Darknov <darknov.dono@gmail.com>
2018-07-31 21:18:11 +0200

    Add simple black curtain before start that exist after clicking start button

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 import { prepareGame } from './game.js';
+import { canvas } from './canvas.js';
 
 startButton.addEventListener("click",() => {
   startButton.hidden = true;
+  // leftCurtain.classList.add("flipLeft");
+  leftCurtain.classList.add("flipLeft");
 	prepareGame();
 });
